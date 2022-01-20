b0f6b690846685141c56b6a8c15453ffac4ce23c
eiei <johndoe@example.com>
2022-01-21 03:12:18 +0900

    seperate fiel

---
+++ hanako.py
@@ -0,0 +1,19 @@
+import sys
+# import threading
+import utils
+from scu import SCU
+
+HANAKO = "169.254.229.153"
+TARO = "169.254.155.219"
+
+def main():
+    # TODO
+    scu = SCU(mtu = 1500)
+    scu.bind_as_receiver(receiver_address = (HANAKO, 8889))
+    for i in range(0, 1000):
+        filedata = scu.recv()
+        utils.write_file(f"./data/data{i}", filedata)
+        print(f"file received: {i}", end="\r")
+
+if __name__ == '__main__':
+    main()
---
+++ taro.py
@@ -0,0 +1,33 @@
+import sys
+# import threading
+import utils
+from scu import SCU
+
+HANAKO = "169.254.229.153"
+TARO = "169.254.155.219"
+
+def main():
+  scu = SCU(mtu=1500)
+  scu.bind_as_sender(receiver_address=(HANAKO, 8889))
+  try:
+      # serial
+      for id in range(0, 1000):
+          scu.send(f"./data/data{id}", id)
+          print(f"file sent: {id}", end="\r")
+
+      # parallel
+      # threads = []
+      # for id in range(0, 1000):
+      #     threads.append(threading.Thread(target = scu.send(f"data/data{id}", id)))
+      #     threads[-1].setDaemon(True)
+      #     threads[-1].start()
+
+      # for th in threads:
+      #     th.join()
+  except Exception as e:
+      print(e)
+      scu.drop() # なくても大丈夫だとは思うけど一応安全のため
+
+
+if __name__ == '__main__':
+    main()
