f3eb0e144714deb24e9902f5634766a7af4d8d3f
Black-Frost <vovantiendung2001@gmail.com>
2022-01-20 21:45:41 +0700

    Fix div by zero error

--- scu.py
+++ scu.py
@@ -107,7 +107,7 @@ class SCU:
                                 lost_packets_send = lost
                                 #seqPos = 0
                             if (last_seq > max_last_seq):
-                                lost_packets_send += [i for i in lost if i > max_last_seq]
+                                lost_packets_send = sorted(lost + lost_packets_send)
                                 max_last_seq = last_seq
                                 #lost_packets_send = lost
 
