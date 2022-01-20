924c406a01418d58354a9e47e620eb9cf75f73de
eiei <johndoe@example.com>
2022-01-21 03:43:52 +0900

    change port

--- hanako.py
+++ hanako.py
@@ -9,7 +9,7 @@ TARO = "169.254.155.219"
 def main():
     # TODO
     scu = SCU(mtu = 1500)
-    scu.bind_as_receiver(receiver_address = (HANAKO, 8889))
+    scu.bind_as_receiver(receiver_address = (HANAKO, 8899))
     for i in range(0, 1000):
         filedata = scu.recv()
         utils.write_file(f"./data/data{i}", filedata)
